b040843c67543524f10531594c9db9ef005ce891
Steve Vinoski <vinoski@ieee.org>
2012-12-07 14:40:51 -0500

    indentation overhaul

--- c_src/syslog_drv.c
+++ c_src/syslog_drv.c
@@ -43,11 +43,11 @@ static ErlDrvTermData am_badarg;
 static ErlDrvTermData am_notopen;
 
 struct syslogdrv {
-	ErlDrvPort port;
-	unsigned char open;
-	char *ident;
-	int logopt;
-	int facility;
+    ErlDrvPort port;
+    unsigned char open;
+    char *ident;
+    int logopt;
+    int facility;
 };
 
 typedef struct syslogdrv syslogdrv_t;
@@ -55,102 +55,102 @@ typedef struct syslogdrv syslogdrv_t;
 static ErlDrvEntry syslogdrv_driver_entry;
 
 static void encode_error(ei_x_buff *buff, char *error) {
-	ei_x_encode_tuple_header(buff, 2);
-	ei_x_encode_atom(buff, "error");
-	ei_x_encode_atom(buff, error);
+    ei_x_encode_tuple_header(buff, 2);
+    ei_x_encode_atom(buff, "error");
+    ei_x_encode_atom(buff, error);
 }
 
 static ErlDrvData syslogdrv_start(ErlDrvPort port, char *buf)
 {
-	syslogdrv_t* d = (syslogdrv_t*)driver_alloc(sizeof(syslogdrv_t));
-	d->port = port;
-	d->open = 0;
-	d->ident = NULL;
-	return (ErlDrvData)d;
+    syslogdrv_t* d = (syslogdrv_t*)driver_alloc(sizeof(syslogdrv_t));
+    d->port = port;
+    d->open = 0;
+    d->ident = NULL;
+    return (ErlDrvData)d;
 }
 
 static void syslogdrv_stop(ErlDrvData handle)
 {
-	driver_free((char*)handle);
+    driver_free((char*)handle);
 }
 
 /* messages from erlang */
 static void syslogdrv_output(ErlDrvData handle, char *buff, ErlDrvSizeT bufflen)
 {
-	syslogdrv_t* d = (syslogdrv_t*)handle;
-	int index = 0, version, arity;
-	ei_x_buff result;
-	char operation[MAXATOMLEN];
+    syslogdrv_t* d = (syslogdrv_t*)handle;
+    int index = 0, version, arity;
+    ei_x_buff result;
+    char operation[MAXATOMLEN];
 
-	ei_x_new_with_version(&result);
+    ei_x_new_with_version(&result);
 	
-	if (ei_decode_version(buff, &index, &version)) {
-		encode_error(&result, "badver");
-		goto done;
-	}
+    if (ei_decode_version(buff, &index, &version)) {
+        encode_error(&result, "badver");
+        goto done;
+    }
 
-	if (ei_decode_tuple_header(buff, &index, &arity) || ei_decode_atom(buff, &index, operation)) {
-		encode_error(&result, "badarg");
-		goto done;
-	}
+    if (ei_decode_tuple_header(buff, &index, &arity) || ei_decode_atom(buff, &index, operation)) {
+        encode_error(&result, "badarg");
+        goto done;
+    }
 
-	if (!strcmp("open", operation)) {
-		int size, type;
+    if (!strcmp("open", operation)) {
+        int size, type;
 
-		if (d->ident) {
-			free(d->ident); /* free old ident string */
-			d->ident = NULL;
-		}
+        if (d->ident) {
+            free(d->ident); /* free old ident string */
+            d->ident = NULL;
+        }
 
-		ei_get_type(buff, &index, &type, &size);
-		if (type == ERL_STRING_EXT) {
-			long logopt, facility;
+        ei_get_type(buff, &index, &type, &size);
+        if (type == ERL_STRING_EXT) {
+            long logopt, facility;
 
-			d->ident = malloc(size+1);
-			ei_decode_string(buff, &index, d->ident);
-			if (ei_decode_long(buff, &index, &logopt) || ei_decode_long(buff, &index, &facility)) {
-				encode_error(&result, "badarg");
-				goto done;
-			}
-			openlog(d->ident, logopt, facility);
-			d->logopt = (int)logopt;
-			d->facility = (int)facility;
-			d->open = 1;
-			ei_x_encode_atom(&result, "ok");
-		} else {
-			encode_error(&result, "badarg");
-		}
-	} else if (!strcmp("log", operation) && d->open) {
-		int size, type;
-		long priority;
-		if (ei_decode_long(buff, &index, &priority)) {
-			encode_error(&result, "badarg");
-			goto done;
-		}
-		ei_get_type(buff, &index, &type, &size);
-		if (type == ERL_STRING_EXT) {
-			char *message;
+            d->ident = malloc(size+1);
+            ei_decode_string(buff, &index, d->ident);
+            if (ei_decode_long(buff, &index, &logopt) || ei_decode_long(buff, &index, &facility)) {
+                encode_error(&result, "badarg");
+                goto done;
+            }
+            openlog(d->ident, logopt, facility);
+            d->logopt = (int)logopt;
+            d->facility = (int)facility;
+            d->open = 1;
+            ei_x_encode_atom(&result, "ok");
+        } else {
+            encode_error(&result, "badarg");
+        }
+    } else if (!strcmp("log", operation) && d->open) {
+        int size, type;
+        long priority;
+        if (ei_decode_long(buff, &index, &priority)) {
+            encode_error(&result, "badarg");
+            goto done;
+        }
+        ei_get_type(buff, &index, &type, &size);
+        if (type == ERL_STRING_EXT) {
+            char *message;
 
-			message = malloc(size+1);
-			ei_decode_string(buff, &index, message);
-			/* re-call openlog in-case another instance of the port driver
-			 * was called in the mean time */
-			openlog(d->ident, d->logopt, d->facility);
-			syslog(priority, "%s", message);
-			free(message);
-			ei_x_encode_atom(&result, "ok");
-		} else {
-			encode_error(&result, "badarg");
-		}
-	} else if (!strcmp("log", operation)) { /* NOT open */
-		encode_error(&result, "notopen");
-	} else {
-		encode_error(&result, "badarg");
-	}
+            message = malloc(size+1);
+            ei_decode_string(buff, &index, message);
+            /* re-call openlog in-case another instance of the port driver
+             * was called in the mean time */
+            openlog(d->ident, d->logopt, d->facility);
+            syslog(priority, "%s", message);
+            free(message);
+            ei_x_encode_atom(&result, "ok");
+        } else {
+            encode_error(&result, "badarg");
+        }
+    } else if (!strcmp("log", operation)) { /* NOT open */
+        encode_error(&result, "notopen");
+    } else {
+        encode_error(&result, "badarg");
+    }
 
-done:
-	driver_output(d->port, result.buff, result.index);
-	ei_x_free(&result);
+ done:
+    driver_output(d->port, result.buff, result.index);
+    ei_x_free(&result);
 }
 
 /*
@@ -159,24 +159,24 @@ done:
 
 DRIVER_INIT(syslogdrv)
 {
-	am_ok           = driver_mk_atom("ok");
-	am_value        = driver_mk_atom("value");
-	am_error        = driver_mk_atom("error");
-	am_badver       = driver_mk_atom("badver");
-	am_badarg        = driver_mk_atom("badarg");
-	am_notopen        = driver_mk_atom("notopen");
+    am_ok      = driver_mk_atom("ok");
+    am_value   = driver_mk_atom("value");
+    am_error   = driver_mk_atom("error");
+    am_badver  = driver_mk_atom("badver");
+    am_badarg  = driver_mk_atom("badarg");
+    am_notopen = driver_mk_atom("notopen");
 
-	syslogdrv_driver_entry.init         = NULL;   /* Not used */
-	syslogdrv_driver_entry.start        = syslogdrv_start;
-	syslogdrv_driver_entry.stop         = syslogdrv_stop;
-	syslogdrv_driver_entry.output       = syslogdrv_output;
-	syslogdrv_driver_entry.ready_input  = NULL;
-	syslogdrv_driver_entry.ready_output = NULL;
-	syslogdrv_driver_entry.driver_name  = "syslog_drv";
-	syslogdrv_driver_entry.finish       = NULL;
-	syslogdrv_driver_entry.outputv      = NULL;
-	syslogdrv_driver_entry.extended_marker = ERL_DRV_EXTENDED_MARKER;
-	syslogdrv_driver_entry.major_version = ERL_DRV_EXTENDED_MAJOR_VERSION;
-	syslogdrv_driver_entry.minor_version = ERL_DRV_EXTENDED_MINOR_VERSION;
-	return &syslogdrv_driver_entry;
+    syslogdrv_driver_entry.init         = NULL;   /* Not used */
+    syslogdrv_driver_entry.start        = syslogdrv_start;
+    syslogdrv_driver_entry.stop         = syslogdrv_stop;
+    syslogdrv_driver_entry.output       = syslogdrv_output;
+    syslogdrv_driver_entry.ready_input  = NULL;
+    syslogdrv_driver_entry.ready_output = NULL;
+    syslogdrv_driver_entry.driver_name  = "syslog_drv";
+    syslogdrv_driver_entry.finish       = NULL;
+    syslogdrv_driver_entry.outputv      = NULL;
+    syslogdrv_driver_entry.extended_marker = ERL_DRV_EXTENDED_MARKER;
+    syslogdrv_driver_entry.major_version = ERL_DRV_EXTENDED_MAJOR_VERSION;
+    syslogdrv_driver_entry.minor_version = ERL_DRV_EXTENDED_MINOR_VERSION;
+    return &syslogdrv_driver_entry;
 }
--- src/syslog.erl
+++ src/syslog.erl
@@ -28,24 +28,24 @@
 
 -define(DRV_NAME, "syslog_drv").
 
-% API
+%% API
 -export([
-	start/0,
-	start_link/0,
-	open/3,
-	log/2,
-	log/3
-]).
+         start/0,
+         start_link/0,
+         open/3,
+         log/2,
+         log/3
+        ]).
 
-% gen_server callbacks
+%% gen_server callbacks
 -export([
-	init/1,
-	handle_call/3,
-	handle_cast/2,
-	handle_info/2,
-	terminate/2,
-	code_change/3
-]).
+         init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3
+        ]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -54,79 +54,79 @@
 -record(state, {port}).
 
 start() ->
-	gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
 
 start_link() ->
-	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 open(Ident, Logopt, Facility) ->
-	gen_server:call(?MODULE, {open, Ident, logopt(Logopt), facility(Facility)}).
+    gen_server:call(?MODULE, {open, Ident, logopt(Logopt), facility(Facility)}).
 
 log(Priority, Message) ->
-	gen_server:call(?MODULE, {log, priorities(Priority), Message}).
+    gen_server:call(?MODULE, {log, priorities(Priority), Message}).
 log(Priority, Message, Timeout) ->
-    	gen_server:call(?MODULE, {log, priorities(Priority), Message}, Timeout).
+    gen_server:call(?MODULE, {log, priorities(Priority), Message}, Timeout).
 
 %%% API %%%
 
 init([]) ->
-	erl_ddll:start(),
-	Path = case code:priv_dir(syslog) of
-		{error, _} ->
-			case load_path(?DRV_NAME++".so") of
-				{error, _} ->
-					error;
-				{ok, P} ->
-					P
-			end;
-		P ->
-			P
-	end,
+    erl_ddll:start(),
+    Path = case code:priv_dir(syslog) of
+               {error, _} ->
+                   case load_path(?DRV_NAME++".so") of
+                       {error, _} ->
+                           error;
+                       {ok, P} ->
+                           P
+                   end;
+               P ->
+                   P
+           end,
 
-	case Path of
-		error ->
-			{stop, no_driver};
-		Path ->
-			case erl_ddll:load_driver(Path, ?DRV_NAME) of
-				ok ->
-					Port = open_port({spawn, ?DRV_NAME}, [binary]),
-					{ok, #state{port = Port}};
-				{error, Error} ->
-					error_logger:format("Error loading driver: " ++ erl_ddll:format_error(Error), []),
-					{stop, bad_driver}
-			end
-	end.
+    case Path of
+        error ->
+            {stop, no_driver};
+        Path ->
+            case erl_ddll:load_driver(Path, ?DRV_NAME) of
+                ok ->
+                    Port = open_port({spawn, ?DRV_NAME}, [binary]),
+                    {ok, #state{port = Port}};
+                {error, Error} ->
+                    error_logger:format("Error loading driver: " ++ erl_ddll:format_error(Error), []),
+                    {stop, bad_driver}
+            end
+    end.
 
 handle_call({log, Priority, Message}, _From, #state{port = Port} = State) ->
-	port_command(Port, erlang:term_to_binary({log, Priority, lists:flatten(Message)})),
-	Reply = receive
+    port_command(Port, erlang:term_to_binary({log, Priority, lists:flatten(Message)})),
+    Reply = receive
 		{Port, {data, Bin}} ->
-			binary_to_term(Bin)
-	after 1000 -> timeout
-	end,
-	{reply, Reply, State};
+                    binary_to_term(Bin)
+            after 1000 -> timeout
+            end,
+    {reply, Reply, State};
 handle_call({open, Ident, Logopt, Facility}, _From, #state{port = Port} = State) ->
-	port_command(State#state.port, erlang:term_to_binary({open, Ident, Logopt, Facility})),
-	Reply = receive
+    port_command(State#state.port, erlang:term_to_binary({open, Ident, Logopt, Facility})),
+    Reply = receive
 		{Port, {data, Bin}} ->
-			binary_to_term(Bin)
-	after 1000 -> timeout
-	end,
-	{reply, Reply, State};
+                    binary_to_term(Bin)
+            after 1000 -> timeout
+            end,
+    {reply, Reply, State};
 handle_call(_Msg, _From, State) ->
-	{reply, ok, State}.
+    {reply, ok, State}.
 
 handle_cast(_Msg, State) ->
-	{noreply, State}.
+    {noreply, State}.
 
 handle_info(_Info, State) ->
-	{noreply, State}.
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
-	ok.
+    ok.
 
 code_change(_, _, _) ->
-	ok.
+    ok.
 
 %%% internal functions %%%
 
@@ -174,30 +174,30 @@ openlog_opt(perror) -> 20;
 openlog_opt(N)      -> N.
 
 logopt([Queue]) -> openlog_opt(Queue);
-logopt([Tail|Queue]) -> 
-	openlog_opt(Tail) bor logopt(Queue);
+logopt([Tail|Queue]) ->
+    openlog_opt(Tail) bor logopt(Queue);
 logopt([]) -> 0;
 logopt(N) -> openlog_opt(N).
 
 load_path(File) ->
-	case lists:zf(fun(Ebin) ->
-					Priv = Ebin ++ "/../priv/",
-					case file:read_file_info(Priv ++ File) of
-						{ok, _} -> {true, Priv};
-						_ -> false
-					end
-			end, code:get_path()) of
-		[Dir|_] ->
-			{ok, Dir};
-		[] ->
-			error_logger:format("Error: ~s not found in code path\n", [File]),
-			{error, enoent}
-	end.
+    case lists:zf(fun(Ebin) ->
+                          Priv = Ebin ++ "/../priv/",
+                          case file:read_file_info(Priv ++ File) of
+                              {ok, _} -> {true, Priv};
+                              _ -> false
+                          end
+                  end, code:get_path()) of
+        [Dir|_] ->
+            {ok, Dir};
+        [] ->
+            error_logger:format("Error: ~s not found in code path\n", [File]),
+            {error, enoent}
+    end.
 
 -ifdef(TEST).
 
 logopt_test() ->
-	{ok, _} = syslog:start(),
-	11 = logopt([1,2,8]).
+    {ok, _} = syslog:start(),
+    11 = logopt([1,2,8]).
 
 -endif.
